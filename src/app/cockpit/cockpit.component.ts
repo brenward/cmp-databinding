6edfcc0de600b61e5c0e9ed8b3128924f0ac9ccb
brenward <bren.ward@gmail.com>
2024-03-18 15:50:36 +0000

    #Using local reference to pass an element to a method

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -8,14 +8,14 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
+  //newServerName = '';
   newServerContent = '';
 
-  onAddServer() {
-    this.serverCreated.emit({serverName:this.newServerName, serverContent:this.newServerContent});
+  onAddServer(nameInput: HTMLInputElement) {
+    this.serverCreated.emit({serverName:nameInput.value, serverContent:this.newServerContent});
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({serverName:this.newServerName, serverContent:this.newServerContent});
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({serverName:nameInput.value, serverContent:this.newServerContent});
   }
 }
